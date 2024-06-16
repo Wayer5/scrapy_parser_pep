ea94049acb3c01bd7efb371baa70693c3faf28eb
Wayer5 <max555.91@mail.ru>
2024-06-16 13:13:29 +0400

    review+dict

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 from pathlib import Path
+from collections import defaultdict
 
 BASE_DIR = Path(__file__).parent.parent
 
@@ -8,14 +9,11 @@ BASE_DIR = Path(__file__).parent.parent
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.status_count = {}
+        self.status_count = defaultdict(int)
 
     def process_item(self, item, spider):
         status = item.get('status')
-        if status in self.status_count:
-            self.status_count[status] += 1
-        else:
-            self.status_count[status] = 1
+        self.status_count[status] += 1
         return item
 
     def close_spider(self, spider):
@@ -24,24 +22,15 @@ class PepParsePipeline:
 
         # Создание файла с результатами по статусам
         path = BASE_DIR / f'results/status_summary_{now}.csv'
-        # with open(path, mode='w', encoding='utf-8') as summary_file:
-        #     writer = csv.writer(summary_file)
-        #     writer.writerow(['Статус', 'Количество'])
-        #     total_count = 0
-        #     for status, count in self.status_count.items():
-        #         writer.writerow([status, count])
-        #         total_count += count
-        #     writer.writerow(['Total', total_count])
+
+        rows = [
+            [status, count]
+            for status, count in self.status_count.items()
+        ]
+
         with open(path, mode='w', encoding='utf-8') as summary_file:
             writer = csv.writer(summary_file)
             writer.writerow(['Статус', 'Количество'])
-
-            # Подготовка данных для записи
-            rows = [
-                [status, count] for status, count in self.status_count.items()
-                ]
-            total_count = sum(self.status_count.values())
-
-            # Запись строк
             writer.writerows(rows)
+            total_count = sum(self.status_count.values())
             writer.writerow(['Total', total_count])
